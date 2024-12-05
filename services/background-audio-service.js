833b08e72f69b75a7051fc75c92e0a7e7dc1a2bf
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-05 15:36:46 +0500

    add background voice

--- services/background-audio-service.js
+++ services/background-audio-service.js
@@ -1,7 +1,7 @@
 const EventEmitter = require('events');
 const { Buffer } = require('node:buffer');
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); 
 
 class BackgroundAudioService extends EventEmitter {
   constructor(streamService) {
