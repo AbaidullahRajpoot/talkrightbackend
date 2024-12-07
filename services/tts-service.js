62341ec179f7ca6ba481704bb8dd6fd0e92b5df4
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-07 12:24:46 +0500

    add background voice

--- services/tts-service.js
+++ services/tts-service.js
@@ -19,13 +19,14 @@ class TextToSpeechService extends EventEmitter {
     try {
       const outputFormat = 'ulaw_8000';
       const response = await fetch(
+        // `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}`,
         `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream`,
         {
           method: 'POST',
           headers: {
             'xi-api-key': process.env.XI_API_KEY,
             'Content-Type': 'application/json',
-            accept: 'audio/wav',
+            accept: 'audio/mpeg',
           },
           body: JSON.stringify({
             text: partialResponse,
