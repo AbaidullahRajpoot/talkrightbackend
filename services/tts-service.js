1b709fd2670ec2dc302e540bd86342ad95a7849f
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-11 11:48:32 +0500

    add

--- services/tts-service.js
+++ services/tts-service.js
@@ -17,26 +17,26 @@ class TextToSpeechService extends EventEmitter {
     if (!partialResponse) { return; }
   
     try {
-      const outputFormat = 'ulaw_8000';
       const response = await fetch(
-        `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}`,
+        `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream`,
         {
-          method: 'POST',
-          headers: {
-            'xi-api-key': process.env.XI_API_KEY,
-            'Content-Type': 'application/json',
-            accept: 'audio/wav',
-          },
-          body: JSON.stringify({
-            text: partialResponse,
-            model_id: "eleven_turbo_v2_5",
-            voice_settings: {
-              stability: 0.75,
-              similarity_boost: 1.0
-            }
-          }),
+            method: 'POST',
+            headers: {
+                'xi-api-key': process.env.XI_API_KEY,
+                'Content-Type': 'application/json',
+                accept: 'audio/mpeg',
+            },
+            body: JSON.stringify({
+                text: partialResponse,
+                model_id: "eleven_turbo_v2_5",
+                voice_settings: {
+                    stability: 0.75,
+                    similarity_boost: 1.0
+                }
+            }),
         }
       );
+
       const audioArrayBuffer = await response.arrayBuffer();
       this.emit('speech', 0, Buffer.from(audioArrayBuffer).toString('base64'), partialResponse, interactionCount);
     } catch (err) {
