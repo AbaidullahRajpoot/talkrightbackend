672b09bc15837ce384aa6bfb37ed1894021dc212
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-11 11:29:25 +0500

    add

--- services/tts-service.js
+++ services/tts-service.js
@@ -17,15 +17,16 @@ class TextToSpeechService extends EventEmitter {
     if (!partialResponse) { return; }
   
     try {
-      const outputFormat = 'ulaw_8000';
+      // const outputFormat = 'ulaw_8000';
       const response = await fetch(
-        `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}`,
+        // `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream?output_format=${outputFormat}`,
+        `https://api.elevenlabs.io/v1/text-to-speech/${this.config.voiceId}/stream`,
         {
           method: 'POST',
           headers: {
             'xi-api-key': process.env.XI_API_KEY,
             'Content-Type': 'application/json',
-            accept: 'audio/wav',
+            accept: 'audio/mpeg',
           },
           body: JSON.stringify({
             text: partialResponse,
