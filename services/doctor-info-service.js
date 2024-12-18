e785f7f2801f8cc63c31a1a91f9a6fd3c8ffa2d7
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-18 11:48:17 +0500

    doctor dynamic

--- services/doctor-info-service.js
+++ services/doctor-info-service.js
@@ -3,18 +3,13 @@ const doctorController = require('../controller/doctorController');
 
 async function getDoctorInfo(doctorName) {
   try {
-    // Use the controller's getDoctorInfo method
-    const result = await doctorController.getDoctorInfo();
-    if (result) {
-      console.log(result);
-      const doctorInfo = result;
-      doctorInfo[doctorName] || { department: "", languages: [], gender: "", shift: "" }
-    } else {
-      return { department: "", languages: [], gender: "", shift: "" };
-    }
+    const doctorInfo = await doctorController.getDoctorInfo();
+    console.log(doctorInfo); // For debugging
+    
+    return doctorInfo[doctorName] || { department: "", languages: [], gender: "", shift: "" };
   } catch (error) {
     console.error('Error fetching doctor info:', error);
-    return { department: "", languages: [], gender: "", shift: "" };cd
+    return { department: "", languages: [], gender: "", shift: "" };
   }
 }
 
